0dbe71a4baf7a0d6310a240024e800cea2242324
hadigun007 <jtgunawan007@gmail.com>
2025-01-31 00:50:12 +0700

    Updates

---
+++ assets/back-BJz2fMR1.js
@@ -0,0 +1,1 @@
+const e="data:image/svg+xml,%3csvg%20width='24'%20height='24'%20viewBox='0%200%2024%2024'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20d='M10.5%2019.5L3%2012M3%2012L10.5%204.5M3%2012H21'%20stroke='%23fff'%20stroke-width='1.5'%20stroke-linecap='round'%20stroke-linejoin='round'/%3e%3c/svg%3e";export{e as b};
